1bd9666c7198e7f56fab519afad7182e8109e9b3
Hugo Duncan <hugo@hugoduncan.org>
2012-10-26 15:48:44 -0400

    Remove test-selectors from project.clj

--- project.clj
+++ project.clj
@@ -8,9 +8,6 @@
                  [com.palletops/hadoop-crate "0.1.0-SNAPSHOT"]
                  [org.cloudhoist/java "0.8.0-SNAPSHOT"]
                  [org.clojure/clojure "1.4.0"]]
-  :test-selectors {:default (complement :live-test)
-                   :live-test :live-test
-                   :all (constantly true)}
   :repositories
   {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"
    "sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
