aca4d58ba7e58cfd200f32828c3bdaca4e155c14
Hugo Duncan <hugo@hugoduncan.org>
2012-12-19 16:56:28 -0500

    Harmonise the jclouds versions

--- profiles.clj
+++ profiles.clj
@@ -107,9 +107,9 @@
  :jclouds
  {:dependencies
   [[org.cloudhoist/pallet-jclouds "1.5.2-SNAPSHOT"]
-   [org.jclouds.provider/aws-ec2 "1.5.2"]
-   [org.jclouds.provider/aws-s3 "1.5.2"]
-   [org.jclouds.driver/jclouds-slf4j "1.5.2"
+   [org.jclouds.provider/aws-ec2 "1.5.3"]
+   [org.jclouds.provider/aws-s3 "1.5.3"]
+   [org.jclouds.driver/jclouds-slf4j "1.5.3"
     ;; the declared version is old and can overrule the resolved version
     :exclusions [org.slf4j/slf4j-api]]
-   [org.jclouds.driver/jclouds-sshj "1.5.2"]]}}
+   [org.jclouds.driver/jclouds-sshj "1.5.3"]]}}
