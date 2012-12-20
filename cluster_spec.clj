2990c75ad9ae55cd2a35aed0be57a02bd426e007
Hugo Duncan <hugo@hugoduncan.org>
2012-12-20 09:37:18 -0500

    Update sample cluster configuration
    Simplifies and annotates the basic cluster_spec.clj sample, and adds a
    cluster_spec_graphite.clj sample with a more complex setup.

--- cluster_spec.clj
+++ cluster_spec.clj
@@ -1,28 +1,34 @@
-(def java-opts {:jmx-authenticate false :jmx-ssl false})
+;;; A simple cluster specification.
 
+;;; The specification is a clojure map.
 
-{:cluster-prefix "hc1"
- :groups {:master
-          {:node-spec {:hardware {:hardware-id "m1.large"
-                                  :min-ram 2048}}
-           :count 1
-           :roles #{:namenode :jobtracker}}
-          :slave
-          {:node-spec {:hardware {:hardware-id "m1.large"
-                                  :min-ram 4096}}
-           :count 2
-           :roles #{:datanode :tasktracker}}
-          ;; :graphite {:count 1 :roles #{:graphite}}
-          }
- :node-spec {:image {:os-family :ubuntu
-                     :os-version-matches "12.04"
-                     :os-64-bit true}}
+{:cluster-prefix "hc1"                ; all nodes will be named with this prefix
+
+ ;; The node-spec here is used to specify hardware and os choices across all
+ ;; types of nodes.
+ :node-spec {:image                     ; used to specify image choices
+             { ;; :image-id can be used to select a specific ami (recommended)
+              :os-family :ubuntu
+              :os-version-matches "12.04" ; this is a regex
+              :os-64-bit true}}
+
+ ;; For each type of node (a group) we want, we can specify its roles, the
+ ;; hardware, etc., and the number of nodes of that type to run.  The node-spec
+ ;; for a group is merged with the common node-spec.
+ :groups
+ {:master                                 ; group name
+  {:node-spec {:hardware                  ; used to specify vm size choices
+               {:hardware-id "m1.large"}} ; "m1.large" is aws-specific
+   :count 1                               ; how many nodes to run in this group
+   :roles #{:namenode :jobtracker}}       ; hadoop roles to run on these nodes
+
+  :slave
+  {:node-spec {:hardware
+               {:hardware-id "m1.large"}}
+   :count 2
+   :roles #{:datanode :tasktracker}}}
+
  :hadoop-settings
  {:dist :cloudera
-  :dist-urls {:cloudera "http://3rd-party-dist.s3.amazonaws.com/"}
-  :config
-  {:namenode (merge {:jmx-port 3000} java-opts)
-   :secondary-namenode (merge {:jmx-port 3001} java-opts)
-   :jobtracker (merge {:jmx-port 3002} java-opts)
-   :datanode (merge {:jmx-port 3003} java-opts)
-   :tasktracker (merge {:jmx-port 3004} java-opts)}}}
+  ;; Use a mirror of cloudera's distribution for greater reliability and speed
+  :dist-urls {:cloudera "http://3rd-party-dist.s3.amazonaws.com/"}}}
---
+++ cluster_spec_graphite.clj
@@ -0,0 +1,36 @@
+;;; A cluster configuration with a Graphite monitor
+
+;;; Common options are factored out to avoid repetition
+(def java-opts
+  {:jmx-authenticate false
+   :jmx-ssl false})
+
+;;; A more complex cluster specification
+{:cluster-prefix "hc2"
+ :groups {:master
+          {:node-spec {:hardware {:min-ram 2048}}
+           :count 1
+           :roles #{:namenode :jobtracker}}
+          :slave
+          {:node-spec {:hardware {:min-ram 4096}}
+           :count 2
+           :roles #{:datanode :tasktracker}}
+          ;; the graphite node will run the graphite web server
+          :graphite
+          {:node-spec {:hardware {:min-ram 2048}}
+           :count 1
+           :roles #{:graphite}}}
+
+ :node-spec {:image {:os-family :ubuntu
+                     :os-version-matches "12.04"
+                     :os-64-bit true}}
+ :hadoop-settings
+ {:dist :cloudera
+  :dist-urls {:cloudera "http://3rd-party-dist.s3.amazonaws.com/"}
+  ;; here we specify role specific configuration of JMX ports
+  :config
+  {:namenode (merge {:jmx-port 3000} java-opts)
+   :secondary-namenode (merge {:jmx-port 3001} java-opts)
+   :jobtracker (merge {:jmx-port 3002} java-opts)
+   :datanode (merge {:jmx-port 3003} java-opts)
+   :tasktracker (merge {:jmx-port 3004} java-opts)}}}
