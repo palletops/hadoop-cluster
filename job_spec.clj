9b7a619a3b16aa798267c9710411d9579716fdcf
Hugo Duncan <hugo@hugoduncan.org>
2012-12-20 10:06:48 -0500

    Annotate the job_spec.clj example

--- job_spec.clj
+++ job_spec.clj
@@ -11,9 +11,18 @@
 ;;     (format "s3n://%s:%s@%s" aws-key aws-secret path)))
 
 {:steps
- [{:jar {:remote-file
-         "/usr/local/hadoop-0.20.2/hadoop-examples-0.20.2-cdh3u5.jar"}
+ [{:jar
+   ;; The jarfile to run can be specified using pallet's remote-file options.
+   ;; To specify a file already on the remote node, use :remote-file to specify
+   ;; its path.
+   ;; To specify a file on the local host, use :local-file to specify the local
+   ;; path.
+   ;; Use :url to specify a jar from the specified url.
+   {:remote-file "/usr/local/hadoop-0.20.2/hadoop-examples-0.20.2-cdh3u5.jar"}
    :main "wordcount"
-   :input (s3n "pallet-play/hadoop-examples")
-   :output (s3n "<your-dest-bucket>/<your-dest-directory>")}]
+   :input (s3n "pallet-play/hadoop-examples")  ; used as first arg if present
+   :output (s3n "<your-dest-bucket>/<your-dest-directory>") ; used as second arg
+                                                            ; if present
+   ;; :args can be used to pass a sequence of command line arguments
+   }]
  :on-completion :terminate-cluster}
