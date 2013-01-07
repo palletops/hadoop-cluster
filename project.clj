ed03f590d934cdb1bad0d374eeab66ed57b66141
Hugo Duncan <hugo@hugoduncan.org>
2013-01-07 12:38:41 -0500

    Update to pallet 0.8.0-alpha.7

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "Hadoop cluster runner"
   :url "https://github.com/palletops/github-cluster"
   :license {:name "All rights reserved."}
-  :dependencies [[org.cloudhoist/pallet "0.8.0-SNAPSHOT"]
+  :dependencies [[org.cloudhoist/pallet "0.8.0-alpha.7"]
                  [com.palletops/collectd-crate "0.1.0"]
                  [com.palletops/graphite-crate "0.1.0"]
-                 [com.palletops/hadoop-crate "0.1.1-SNAPSHOT"]
-                 [com.palletops/hadoop-config "0.1.0-SNAPSHOT"]
+                 [com.palletops/hadoop-crate "0.1.1"]
+                 [com.palletops/hadoop-config "0.1.0"]
                  [org.cloudhoist/java "0.8.0-alpha.1"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/clojure "1.4.0"]]
