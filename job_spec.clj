6d8eb3055005da40a108ef7ac8faa7ef14e3f447
Hugo Duncan <hugo@hugoduncan.org>
2013-01-07 17:02:34 -0500

    Update job_spec.clj to remove credentials

--- job_spec.clj
+++ job_spec.clj
@@ -1,8 +1,11 @@
-;;; Put you s3 credentials in environment variables
-(defn s3n [path]
-  (let [aws-key <your-aws-s3-key>
-        aws-secret <your-aws-s3-secret>]
-    (format "s3n://%s:%s@%s" aws-key aws-secret path)))
+;;; If you want non-cluster wide credentials, put you s3 credentials in a
+;;; function to generate urls with credntials. Note that this doesn't work
+;;; with credentials containing a "/".
+
+;; (defn s3n [path]
+;;   (let [aws-key <your-aws-s3-key>
+;;         aws-secret <your-aws-s3-secret>]
+;;     (format "s3n://%s:%s@%s" aws-key aws-secret path)))
 
 ;;; As an alternative, you can specify s3 credentials via environment variables
 ;; (defn s3n [path]
@@ -20,9 +23,9 @@
    ;; Use :url to specify a jar from the specified url.
    {:remote-file "/usr/local/hadoop-0.20.2/hadoop-examples-0.20.2-cdh3u5.jar"}
    :main "wordcount"
-   :input (s3n "pallet-play/hadoop-examples")  ; used as first arg if present
-   :output (s3n "<your-dest-bucket>/<your-dest-directory>") ; used as second arg
-                                                            ; if present
+   :input ("s3n://pallet-play/hadoop-examples") ; used as first arg if present
+   :output ("s3n://<your-dest-bucket>/<your-dest-directory>") ; used as second
+                                                              ; arg if present
    ;; :args can be used to pass a sequence of command line arguments
    }]
  :on-completion :terminate-cluster}
