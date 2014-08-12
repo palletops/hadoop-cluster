342ac1d2534af581fe811b5d642180142c1b9fcb
Antoni Batchelli <tbatchelli@acm.org>
2014-08-12 11:48:31 -0700

    Change ownership of downloaded .jar in example job to hadoop:hadoop

--- job_spec.clj
+++ job_spec.clj
@@ -8,7 +8,10 @@
    ;; To specify a file on the local host, use :local-file to specify the local
    ;; path.
    ;; Use :url to specify a jar from the specified url.
-   {:remote-file "/usr/local/hadoop-0.20.2/hadoop-examples-0.20.2-cdh3u5.jar"}
+   {:remote-file "/usr/local/hadoop-0.20.2/hadoop-examples-0.20.2-cdh3u5.jar"
+    :owner "hadoop" ;file permissions necessary if using local-file else it's set to the local user launching the job
+    :group "hadoop
+    }
    :main "wordcount"
    :input "s3n://pallet-play/hadoop-examples" ; used as first arg if present
    :output "s3n://<your-dest-bucket>/<your-dest-directory>" ; used as second
